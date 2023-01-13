6879995a5fabc8cc7a8aa4495a67dfcd7dc99427
bpkr95 <122605995+bpkr95@users.noreply.github.com>
2023-01-13 10:37:16 +0000

    adding code with dynamic blocks for functional

---
+++ modules/functional/main.tf
@@ -0,0 +1,218 @@
+module "azure_region" {
+  source  = "claranet/regions/azurerm"
+  version = ">=6"
+  azure_region = var.location
+}
+
+resource "azurerm_function_app" "function_app" {
+  # Required   
+  name                       = local.name
+  location                   = module.azure_region.location
+  resource_group_name        = var.resource_group_name
+  app_service_plan_id        = var.app_service_plan_id
+  storage_account_name       = var.storage_account_name
+  storage_account_access_key = var.storage_account_primary_access_key
+  
+  # Optional
+  app_settings               = var.app_settings
+  tags                       = var.tags
+  version                    = var.funcion_app_version
+
+  dynamic site_config {
+    for_each = var.site_config != null ? var.site_config : []
+    content {
+      always_on                         = try(site_config.value.function_app_always_on, false)
+      app_scale_limit                   = try(site_config.value.app_scale_limit, null) # Only applicable to apps on the Consumption and Premium plan.
+      
+      dynamic cors {
+        for_each = site_config.value.cors != null ? site_config.value.cors : []
+        content {
+          allowed_origins = cors.value.allowed_origins # Required
+          support_credentials = try(cors.support_credentials, false)
+        }
+      }
+
+      dotnet_framework_version          = try(site_config.value.dotnet_framework_version, "v4.0")
+      elastic_instance_minimum          = try(site_config.value.elastic_instance_minimum, null)
+      ftps_state                        = try(site_config.value.ftps_state, "AllAllowed")
+      health_check_path                 = try(site_config.value.health_check_path, null)
+      http2_enabled                     = try(site_config.value.http2_enabled, false)
+
+      # ip_restriction {
+      #   ip_address  = "0.0.0.0/0"
+      #   name = "Deny All"
+      #   action = "Deny"
+      #   priority = 1000
+      # }
+
+      dynamic ip_restriction {
+        for_each = site_config.value.ip_restriction != null ? site_config.value.ip_restriction : []
+        content {
+          ip_address = try(ip_restriction.value.ip_address, null)
+          service_tag = try(ip_restriction.value.service_tag, null)
+          virtual_network_subnet_id = try(ip_restriction.value.virtual_network_subnet_id, null)
+          name = try(ip_restriction.value.name, null)
+          priority = try(ip_restriction.value.priority, 65000)
+          action = try(ip_restriction.value.action, "Allow")
+          dynamic "headers" {
+            for_each = ip_restriction.value.headers != null ? ip_restriction.value.headers : []
+            content {
+              x_azure_fdid  = try(headers.value.x_azure_fdid, [])
+              x_fd_health_probe = try(headers.value.x_fd_health_probe, [])
+              x_forwarded_for = try(headers.value.x_forwarded_for, [])
+              x_forwarded_host = try(headers.value.x_forwarded_host, [])
+            }
+          }
+        }
+      }
+
+      # scm_ip_restriction {
+      #   ip_address  = "0.0.0.0/0"
+      #   name = "Deny All"
+      #   action = "Deny"
+      #   priority = 1000
+      # }
+      
+      dynamic scm_ip_restriction {
+        for_each = site_config.value.scm_ip_restriction != null ? site_config.value.scm_ip_restriction : []
+        content {
+          ip_address = try(scm_ip_restriction.value.ip_address, null)
+          service_tag = try(scm_ip_restriction.value.service_tag, null)
+          virtual_network_subnet_id = try(scm_ip_restriction.value.virtual_network_subnet_id, null)
+          name = try(scm_ip_restriction.value.name, null)
+          priority = try(scm_ip_restriction.value.priority, 65000)
+          action = try(scm_ip_restriction.value.action, "Allow")
+          dynamic "headers" {
+            for_each = scm_ip_restriction.value.headers != null ? scm_ip_restriction.value.headers : []
+            content {
+              x_azure_fdid  = try(headers.value.x_azure_fdid, [])
+              x_fd_health_probe = try(headers.value.x_fd_health_probe, [])
+              x_forwarded_for = try(headers.value.x_forwarded_for, [])
+              x_forwarded_host = try(headers.value.x_forwarded_host, [])
+            }
+          }
+        }
+      }
+
+      java_version = try(site_config.value.java_version, null)
+      linux_fx_version = try(site_config.value.linux_fx_version, null)
+      min_tls_version  = try(site_config.value.min_tls_version)
+      pre_warmed_instance_count = try(site_config.value.pre_warmed_instance_count, null)
+      runtime_scale_monitoring_enabled  = try(site_config.value.runtime_scale_monitoring_enabled, false)
+      scm_type = try(site_config.value.scm_type, "None")
+      scm_use_main_ip_restriction = try(site_config.value.scm_use_main_ip_restriction, false)
+      use_32_bit_worker_process         = try(site_config.value.use_32_bit_worker_process, true)  
+      vnet_route_all_enabled  = try(site_config.value.vnet_route_all_enabled, false)
+      websockets_enabled = try(site_config.value.websockets_enabled, false)
+    }
+  }
+  
+  # identity {
+  #   type         = var.identity_type
+  # }
+
+  dynamic identity {
+    for_each = var.identity != null ? var.identity : []
+    content {
+      type = identity.value.type
+      identity_ids = try(identity.value.identity_ids, [])
+    }
+  }
+
+  dynamic auth_settings {
+    for_each = var.auth_settings != null ? var.auth_settings : []
+    content {
+      enabled = auth_settings.value.enabled
+      additional_login_params = try(auth_settings.value.additional_login_params, null)
+      allowed_external_redirect_urls = try(auth_settings.value.allowed_external_redirect_urls, null)
+      default_provider = try(auth_settings.value.default_provider, "AzureActiveDirectory")
+      issuer = try(auth_settings.value.issuer, null)
+      runtime_version = try(auth_settings.value.runtime_version, null)
+      token_refresh_extension_hours = try(auth_settings.value.token_refresh_extension_hours, 72)
+      token_store_enabled = try(auth_settings.value.token_store_enabled, false)
+      unauthenticated_client_action = try(auth_settings.value.unauthenticated_client_action, "AllowAnonymous")
+
+      dynamic active_directory {
+        for_each = auth_settings.value.active_directory != null ? auth_settings.value.active_directory : []
+        content {
+          client_id = active_directory.value.client_id
+          client_secret = active_directory.value.client_secret
+          allowed_audiences = active_directory.value.allowed_audiences
+        }
+      }
+
+      dynamic facebook {
+        for_each = auth_settings.value.facebook != null ? auth_settings.value.facebook : []
+        content {
+          app_id = facebook.value.app_id
+          app_secret = facebook.value.app_secret
+          oauth_scopes = facebook.value.oauth_scopes
+        }
+      }
+
+      dynamic google {
+        for_each = auth_settings.value.google != null ? auth_settings.value.google : []
+        content {
+          client_id = google.value.client_id
+          client_secret = google.value.client_secret
+          oauth_scopes = google.value.oauth_scopes
+        }
+      }
+
+      dynamic microsoft {
+        for_each = auth_settings.value.microsoft != null ? auth_settings.value.microsoft : []
+        content {
+          client_id = microsoft.value.client_id
+          client_secret = microsoft.value.client_secret
+          oauth_scopes = microsoft.value.oauth_scopes
+        }
+      }
+
+       dynamic twitter {
+        for_each = auth_settings.value.twitter != null ? auth_settings.value.twitter : []
+        content {
+          consumer_key = twitter.value.consumer_key
+          consumer_secret = twitter.value.consumer_key
+        }
+      }
+
+
+      
+    }
+  }
+
+  dynamic source_control {
+    for_each = var.source_control != null ? var.source_control : []
+    content {
+      repo_url = try(source_control.value.repo_url, null)
+      branch   = try(source_control.value.branch, "master")
+      manual_integration = try(source_control.value.manual_integration, false)
+      rollback_enabled = try(source_control.value.rollback_enabled, false)
+      use_mercurial = try(source_control.value.use_mercurial, false)
+
+    }
+  }
+}
+
+
+resource "azurerm_monitor_diagnostic_setting" "monitor_diagnostic_setting" {
+  count                      = var.enable_functionapp_diagnostic ? 1 : 0
+  name                       = "${local.name}-diagnostics"
+  target_resource_id         = azurerm_function_app.function_app.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  log {
+    category = "FunctionAppLogs"
+    enabled  = true
+    retention_policy {
+      enabled = true
+    }
+  }
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = true
+    }
+  }
+}
