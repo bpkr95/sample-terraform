1a65b80c533609e051e73989fea41d99df005edf
bpkr95 <122605995+bpkr95@users.noreply.github.com>
2023-01-13 10:34:57 +0000

    adding project variables.tf

---
+++ project/variables.tf
@@ -0,0 +1,3 @@
+variable "main_project"                                 { default = "elsevier" }
+variable "sub_project"                                  { default = "01" }
+variable "location"                                     { default = "uksouth" } 
